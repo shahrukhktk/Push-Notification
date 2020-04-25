1b73b7bc21973c27c130ed82d0cc5c8d2ac31297
Shahrukh <Shahrukhktk@gmail.com>
2020-04-25 14:05:27 +0500

    first commit

---
+++ app/src/main/java/com/devigntech/pushnotificationandroid/MyMessagingService.java
@@ -0,0 +1,30 @@
+package com.devigntech.pushnotificationandroid;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyMessagingService extends FirebaseMessagingService
+{
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage)
+    {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    public void showNotification(String title, String Message)
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Notifications")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(Message);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(999, builder.build());
+    }
+
+}
